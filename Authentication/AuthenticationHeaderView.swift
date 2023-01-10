7f4d193840a5097a84133d4d9cb281fdf34af59f
Radu <radu_bila@yahoo.com>
2023-01-10 12:50:31 +0200

    Added animation

--- Authentication/AuthenticationHeaderView.swift
+++ Authentication/AuthenticationHeaderView.swift
@@ -11,6 +11,7 @@ struct AuthenticationHeaderView: View {
     // MARK: - PROPERTIES
     let title1: String
     let title2: String
+    @State var show = false
     
     // MARK: - BODY
     var body: some View {
@@ -32,6 +33,37 @@ struct AuthenticationHeaderView: View {
         }
         .frame(height: 260)
         .padding(.leading)
+        .background(
+            ZStack{
+                Image("Blob1")
+                    .resizable()
+                    .offset(x: -50, y: -70)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 600, height: 600)
+                    .rotationEffect(Angle(degrees: show ? 360 + 90 : 90))
+                    .blendMode(.overlay)
+                    .animation(Animation.linear(duration: 60).repeatForever())
+                    .onAppear {
+                        self.show = true
+                    }
+                Image("Blob3")
+                    .resizable()
+                    .offset(x: -90, y: -100)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 350, height: 350)
+                    .rotationEffect(Angle(degrees: show ? -90 : 0), anchor: .topTrailing)
+                    .blendMode(.overlay)
+                    .animation(Animation.linear(duration: 50).repeatForever())
+                Image("Blob2")
+                    .resizable()
+                    .offset(x: -90, y: -100)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 400, height: 400)
+                    .rotationEffect(Angle(degrees: show ? 130 : 0), anchor: .top)
+                    .blendMode(.overlay)
+                    .animation(Animation.linear(duration: 40).repeatForever())
+            }
+        )
         .background(Color("AccentColor"))
         .foregroundColor(.white)
         .clipShape(RoundedShape(corners: [.bottomRight], cornerRadius: 80))
