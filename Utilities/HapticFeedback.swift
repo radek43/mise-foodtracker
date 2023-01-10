ce848d69253aa05cbb9844c8bc46cfb254a4b71f
Radu <radu_bila@yahoo.com>
2023-01-10 12:51:59 +0200

    Added Haptic Feedback

---
+++ Utilities/HapticFeedback.swift
@@ -0,0 +1,16 @@
+//
+//  HapticFeedback.swift
+//  maiIncercSiEu
+//
+//  Created by Radu Bila on 1/9/23.
+//
+
+import SwiftUI
+
+func haptic(type: UINotificationFeedbackGenerator.FeedbackType) {
+    UINotificationFeedbackGenerator().notificationOccurred(type)
+}
+
+func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+    UIImpactFeedbackGenerator(style: style).impactOccurred()
+}
