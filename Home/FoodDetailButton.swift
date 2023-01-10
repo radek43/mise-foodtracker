17f1814a15a10096ab959e298c402910c41b2a90
Radu <radu_bila@yahoo.com>
2023-01-10 18:12:56 +0200

    Added button for changing meal properties

---
+++ Home/FoodDetailButton.swift
@@ -0,0 +1,52 @@
+//
+//  FoodDetailButton.swift
+//  maiIncercSiEu
+//
+//  Created by Radu Bila on 1/10/23.
+//
+
+import SwiftUI
+
+struct FoodDetailButton: View {
+    // MARK: - PROPERTIES
+    var imageName: String
+    var propertyTitle: String
+    var propertyValue: String
+    
+    init(imageName: String, propertyTitle: String, propertyValue: String) {
+        self.imageName = imageName // assign all the parameters, not only `content`
+        self.propertyTitle = propertyTitle
+        self.propertyValue = propertyValue
+
+    }
+    // MARK: - BODY
+    var body: some View {
+            VStack {
+                Circle()
+                    .stroke(Color("ButtonColor"), style: StrokeStyle(lineWidth: 5))
+                    .frame(width: 48, height: 48)
+                    .foregroundColor(Color(UIColor.label))
+                    .background(Circle().fill(Color("ButtonColor")))
+                    .clipShape(Circle())
+                    .overlay(
+                        Image(imageName)
+                            .font(Font.title.weight(.light))
+                    )
+                Text(propertyTitle)
+                    .font(.footnote)
+                    .foregroundColor(Color.primary)
+                    .multilineTextAlignment(.center)
+                Text(propertyValue)
+                    .foregroundColor(Color.primary)
+                    .multilineTextAlignment(.center)
+
+            }
+            .padding(9)
+    }
+}
+
+struct FoodDetailButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FoodDetailButton(imageName: "pastaDish", propertyTitle: "nr. portii", propertyValue: "3.0")
+    }
+}
