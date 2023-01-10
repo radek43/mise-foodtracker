4b82217f7ea196f2a0b5571dbab0572911239eca
Radu <radu_bila@yahoo.com>
2023-01-10 18:12:08 +0200

    Alignment fix

--- Recipes/RecipesView.swift
+++ Recipes/RecipesView.swift
@@ -14,53 +14,58 @@ struct RecipesView: View {
 
     // MARK: - BODY
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            VStack {
-                HStack(alignment: .center, spacing: 3.0) {
-                    SearchBar(placeholderText: "Gaseste-ți inspirația",text: $viewModel.searchText)
-                        .padding(.bottom, 3)
-                    Button {
-                        print("buton setari retete apasat")
-                    } label: {
-                        Image(systemName: "slider.horizontal.3")
-                            .imageScale(.large)
+        ZStack {
+            Color("ColorBackground")
+                .edgesIgnoringSafeArea(.all)
+            
+            ScrollView(showsIndicators: false) {
+                VStack {
+                    HStack(alignment: .center) {
+                        SearchBar(placeholderText: "Gaseste-ți inspirația",text: $viewModel.searchText)
+                            .padding(.bottom, 3)
+                        Button {
+                            print("buton setari retete apasat")
+                        } label: {
+                            Image(systemName: "slider.horizontal.3")
+                                .imageScale(.large)
+                        }
                     }
+                    .padding(.bottom, 5.0)
+                    
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 150), spacing: 15)], spacing: 15) {
+                        ForEach(viewModel.searchableRecipes) { recipe in
+                            NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
+                                RecipeCard(recipe: recipe)
+                            }
+                        }
+                    }
+                    .padding(.bottom)
+                    
+                    Spacer()
                 }
-                .padding(.bottom, 5.0)
-                
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 150), spacing: 15)], spacing: 15) {
-                    ForEach(viewModel.searchableRecipes) { recipe in
-                        NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
-                            RecipeCard(recipe: recipe)
+                .frame(maxWidth: 612)
+                .padding(.horizontal)
+            }
+            .toolbar {
+                if authViewModel.currentUser?.userType == 1 { // functionalitati de admin
+                    HStack {
+                        NavigationLink {
+                            AddNewRecipe() // de modificat
+                        } label: {
+                            Image(systemName: "note.text.badge.plus")
                         }
                     }
                 }
-                .padding(.bottom)
-                
-                Spacer()
             }
-            .padding(.horizontal)
+            .navigationTitle("Rețete")
         }
-        .toolbar {
-            if authViewModel.currentUser?.userType == 1 { // functionalitati de admin
-                HStack {
-                    NavigationLink {
-                        AddNewRecipe() // de modificat
-                    } label: {
-                        Image(systemName: "note.text.badge.plus")
-                    }
-                }
-            }
-        }
-        .navigationTitle("Rețete")
-        .background(Color("ColorBackground").edgesIgnoringSafeArea(.all))
     }
 }
 
 // MARK: - PREVIEW
 struct RecipesView_Previews: PreviewProvider {
     static var previews: some View {
-        TabBar()
+        RecipesView()
             .environmentObject(AuthViewModel())
     }
 }
