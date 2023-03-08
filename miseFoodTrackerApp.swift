4e0e4aad38b424364593d322b236ef676886310c
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-03-08 18:14:09 +0200

    file rename

---
+++ miseFoodTrackerApp.swift
@@ -0,0 +1,23 @@
+//
+//  maiIncercSiEuApp.swift
+//  maiIncercSiEu
+//
+//  Created by Radu Bila on 4/9/22.
+//
+
+import SwiftUI
+
+
+@main
+struct miseFoodTrackerApp: App {
+    // MARK: - PROPERTIES
+//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+//    @StateObject var viewModel = AuthViewModel()
+
+    // MARK: - BODY
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
