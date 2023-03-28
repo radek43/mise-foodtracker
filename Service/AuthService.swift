738c67ab947f1cff2d07b0729d8f3e0cd9c4c543
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-03-28 18:36:28 +0300

    added fetchUser function

--- Service/AuthService.swift
+++ Service/AuthService.swift
@@ -21,10 +21,11 @@ enum AuthenticationError: Error {
 }
 
 
+
 public class AuthServices {
     // MARK: - PROPERTIES
     public static var requestDomain = ""
-    
+    static var keychainService = KeychainService()
     
     // MARK: - METHODS
     
@@ -57,11 +58,12 @@ public class AuthServices {
     static func makeRequest(urlString: String, reqBody: [String: Any], authorization: String? = nil, completion: @escaping (_ result: Result<Data?, NetworkError>) -> Void) {
         
         guard let url = URL(string: urlString) else {
-            print("DEBUG: API is not available")
+            print("makeRequest: API is not available")
             return
         }
         
         var request = URLRequest(url: url)
+        
         request.httpMethod = "POST"
         
         do {
@@ -70,13 +72,11 @@ public class AuthServices {
             print(error.localizedDescription)
         }
              
-            
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         if let authorization = authorization {
             request.addValue("Token \(authorization)", forHTTPHeaderField: "Authorization")
         }
-        
         
         let task = URLSession.shared.dataTask(with: request) { data, res, err in
             guard err == nil else { return }
@@ -98,8 +98,56 @@ public class AuthServices {
         task.resume()
     }
     
-    static func fetchUser(email: String, completion: @escaping (_ result: Result<Data, AuthenticationError>) -> Void) {
-        let urlString = ""
+    static func fetchUser(email: String, completion: @escaping (_ result: Result<Data?, AuthenticationError>) -> Void) {
+        var token = ""
+        
+        // get token from Keychain
+        do {
+            guard let keychainResult = (try self.keychainService.get(service: "mise-foodtracker", account: email)) else {
+                print("KeychainService: Failed to read token.")
+                return
+            }
+            token = String(decoding: keychainResult, as: UTF8.self)
+        } catch {
+            print("fetchUser:\(error)")
+        }
+        
+        let urlString = "http://127.0.0.1:8000/api/user/me/"
+        
+        guard let url = URL(string: urlString) else {
+            print("fetchUser: API is not available")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "GET"
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("Token \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Request error: ", error)
+                return
+                
+            }
+            guard let data = data else {
+                completion(.failure(.invalidCredentials))
+                return
+            }
+            
+            completion(.success(data))
+            
+            do {
+                if let json = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {}
+                
+            } catch let error {
+                completion(.failure(.invalidCredentials))
+                print(error)
+            }
+        }
+        task.resume()
     }
-    
 }
