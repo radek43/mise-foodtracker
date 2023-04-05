37c1c2b080b005b30e0094729373f009e5ea92ea
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-05 19:06:39 +0300

    added navigationView from tabView to view

--- Home/HomeView.swift
+++ Home/HomeView.swift
@@ -12,168 +12,164 @@ import SwiftUICharts
 
 struct HomeView: View {
     // MARK: - PROPERTIES
+    @EnvironmentObject var viewModel: AuthViewModel
     @State var uiTabarController: UITabBarController?
     @State private var data = Date()
     @State var showDishSheet = false
+
     
     // MARK: - BODY
     var body: some View {
-        ZStack {
-            Color.background
-                .edgesIgnoringSafeArea(.all)
-            
-            ScrollView(showsIndicators: false) {
-                // USER GREETING
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text("\(Greetings()), Radu")
-                            .font(.system(.title, design: .rounded))
-                            .fontWeight(.semibold)
-                        Spacer()
-                    }
-                }
-                .padding(.top, 35)
-                .padding(.horizontal)
-                
-                // DATE PICKER
-                VStack {
-                    DatePicker("Data:", selection: $data, in: ...Date(), displayedComponents: .date)
-                        .accentColor(.primary)
-                }
-                .card()
-                
-                // USER OBJECTIVE
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text("Obiectiv")
-                            .font(.title3)
-                            .fontWeight(.semibold)
-                        Spacer()
-                        Text("2342 kCal")
-                            .font(.body)
-                            .fontWeight(.semibold)
-                    }
-                    HStack {
-                        Spacer()
-                        Text("2342 kCal Ramase")
-                            .fontWeight(.semibold)
-                            .padding(5)
-                        Spacer()
-                    }
-                    ProgressView(value: 0.25)
+        if let user = viewModel.currentUser {
+            NavigationView {
+                ZStack {
+                    Color.background
+                        .edgesIgnoringSafeArea(.all)
                     
-                    HStack {
+                    ScrollView(showsIndicators: false) {
+                        // DATE PICKER
                         VStack {
-                            Text("0")
-                                .font(.title3)
-                                .fontWeight(.medium)
-                            Text("Consumat")
-                                .font(.subheadline)
-                                .fontWeight(.medium)
+                            DatePicker("Data:", selection: $data, in: ...Date(), displayedComponents: .date)
+                                .accentColor(.primary)
                         }
-                        .frame(maxWidth: .infinity)
+                        .card()
+                        .padding(.top)
                         
-                        VStack {
-                            Text("0")
-                                .font(.title3)
-                                .fontWeight(.medium)
-                            Text("Ars")
-                                .font(.subheadline)
-                                .fontWeight(.medium)
+                        // USER OBJECTIVE
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text("Obiectiv")
+                                    .font(.title3)
+                                    .fontWeight(.semibold)
+                                Spacer()
+                                Text("2342 kCal")
+                                    .font(.body)
+                                    .fontWeight(.semibold)
+                            }
+                            HStack {
+                                Spacer()
+                                Text("2342 kCal Ramase")
+                                    .fontWeight(.semibold)
+                                    .padding(5)
+                                Spacer()
+                            }
+                            ProgressView(value: 0.25)
+                            
+                            HStack {
+                                VStack {
+                                    Text("0")
+                                        .font(.title3)
+                                        .fontWeight(.medium)
+                                    Text("Consumat")
+                                        .font(.subheadline)
+                                        .fontWeight(.medium)
+                                }
+                                .frame(maxWidth: .infinity)
+                                
+                                VStack {
+                                    Text("0")
+                                        .font(.title3)
+                                        .fontWeight(.medium)
+                                    Text("Ars")
+                                        .font(.subheadline)
+                                        .fontWeight(.medium)
+                                }
+                                .frame(maxWidth: .infinity)
+                                
+                                VStack {
+                                    Text("0")
+                                        .font(.title3)
+                                        .fontWeight(.medium)
+                                    Text("Net")
+                                        .font(.subheadline)
+                                        .fontWeight(.medium)
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
                         }
-                        .frame(maxWidth: .infinity)
+                        .card()
                         
-                        VStack {
-                            Text("0")
+                        // LOG EATEN FOOD
+                        VStack(alignment: .leading) {
+                            Text("Jurnal")
                                 .font(.title3)
-                                .fontWeight(.medium)
-                            Text("Net")
-                                .font(.subheadline)
-                                .fontWeight(.medium)
+                                .fontWeight(.semibold)
+                            
+                            HStack(alignment: .top, spacing: 0.0) {
+                                Spacer()
+                                Button {
+                                    self.showDishSheet.toggle()
+                                } label: {
+                                    AddDishButton(imageName: "breakfast", title: "mic\ndejun")
+                                }
+                                .sheet(isPresented: $showDishSheet) {
+                                    DishDetailConfirm()
+                                }
+                                Spacer()
+                                Button {
+                                    print("Buton pranz apasat")
+                                } label: {
+                                    AddDishButton(imageName: "soupLadle", title: "prânz")
+                                }
+                                Spacer()
+                                // RingView()
+                                Button {
+                                    print("Buton cina apasat")
+                                } label: {
+                                    AddDishButton(imageName: "pastaDish", title: "cină")
+                                }
+                                Spacer()
+                                Button {
+                                    print("Am apasat")
+                                } label: {
+                                    AddDishButton(imageName: "icecream", title: "gustări")
+                                }
+                                Spacer()
+                            }
+                            
+                            HStack(alignment: .top, spacing: 0.0) {
+                                Spacer()
+                                Spacer()
+                                Button {
+                                    print("Buton sport apasat")
+                                } label: {
+                                    AddDishButton(imageName: "sprint", title: "sport")
+                                }
+                                Spacer()
+                                Button {
+                                    print("Buton greutate apasat")
+                                } label: {
+                                    AddDishButton(imageName: "weightScale", title: "greutate")
+                                }
+                                Spacer()
+                                Button {
+                                    print("Buton lichide apasat")
+                                } label: {
+                                    AddDishButton(imageName: "waterDrop", title: "lichide")
+                                }
+                                Spacer()
+                                Spacer()
+                            }
                         }
-                        .frame(maxWidth: .infinity)
-                    }
-                }
-                .card()
-                
-                // LOG EATEN FOOD
-                VStack(alignment: .leading) {
-                    Text("Jurnal")
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                    
-                    HStack(alignment: .top, spacing: 0.0) {
-                        Spacer()
-                        Button {
-                            self.showDishSheet.toggle()
-                        } label: {
-                            AddDishButton(imageName: "breakfast", title: "mic\ndejun")
-                        }
-                        .sheet(isPresented: $showDishSheet) {
-                            DishDetailConfirm()
-                        }
-                        Spacer()
-                        Button {
-                            print("Buton pranz apasat")
-                        } label: {
-                            AddDishButton(imageName: "soupLadle", title: "prânz")
-                        }
-                        Spacer()
-                        // RingView()
-                        Button {
-                            print("Buton cina apasat")
-                        } label: {
-                            AddDishButton(imageName: "pastaDish", title: "cină")
-                        }
-                        Spacer()
-                        Button {
-                            print("Am apasat")
-                        } label: {
-                            AddDishButton(imageName: "icecream", title: "gustări")
-                        }
-                        Spacer()
-                    }
-                    
-                    HStack(alignment: .top, spacing: 0.0) {
-                        Spacer()
-                        Spacer()
-                        Button {
-                            print("Buton sport apasat")
-                        } label: {
-                            AddDishButton(imageName: "sprint", title: "sport")
-                        }
-                        Spacer()
-                        Button {
-                            print("Buton greutate apasat")
-                        } label: {
-                            AddDishButton(imageName: "weightScale", title: "greutate")
-                        }
-                        Spacer()
-                        Button {
-                            print("Buton lichide apasat")
-                        } label: {
-                            AddDishButton(imageName: "waterDrop", title: "lichide")
-                        }
-                        Spacer()
-                        Spacer()
-                    }
-                }
-                .card()
-                
-                DailyCaloriesChart()
-                
-            } //: END SCROLL VIEW
-            .navigationTitle("Pagina principala")
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationBarItems(
-                trailing:
-                    NavigationLink {
-                        SettingsView(viewModel: AuthViewModel.shared)
-                    } label: {
-                        Image(systemName: "gearshape")
-                    }
-            )
-        } //: END ZSTACK
+                        .card()
+                        
+                        DailyCaloriesChart()
+                            .padding(.bottom, 20)
+                        
+                    } //: END SCROLL VIEW
+                    .navigationTitle("Pagina Principală")
+                    .navigationBarItems(
+                        trailing:
+                            NavigationLink {
+                                SettingsView()
+                            } label: {
+                                Image(systemName: "gearshape")
+                            }
+                    )
+                } //: END ZSTACK
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+        }
     }
 }
 
@@ -181,17 +177,13 @@ struct HomeView: View {
 // MARK: - PREVIEW
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            NavigationView {
-                HomeView()
-            }
-            .navigationViewStyle(StackNavigationViewStyle())
-            NavigationView {
-                HomeView()
-                    .preferredColorScheme(.dark)
-            }
-            .navigationViewStyle(StackNavigationViewStyle())
+        let viewModel = AuthViewModel()
+        viewModel.currentUser = userPreviewData
+        return Group {
+            HomeView()
+            HomeView()
+                .preferredColorScheme(.dark)
         }
-        //      .environmentObject(AuthViewModel())
+        .environmentObject(viewModel)
     }
 }
