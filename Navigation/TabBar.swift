05977fb0a962bcf0bc0482ca013639d6a5d877bd
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-05 19:06:07 +0300

    remade previews

--- Navigation/TabBar.swift
+++ Navigation/TabBar.swift
@@ -13,56 +13,53 @@ struct TabBar: View {
     
     @State private var tabSelection = 0
     
+    init() {
+        UITabBar.appearance().barTintColor = UIColor(Color.mono)
+        UITabBar.appearance().backgroundColor = UIColor(Color.mono)
+       }
+    
     // MARK: - BODY
     var body: some View {
         TabView(selection: $tabSelection) {
-            NavigationView {
-                HomeView()
-            }
-            .tabItem {
-                if tabSelection == 0 { Image("house.fill") }
-                else { Image("house") }
-                Text("Meniu")
-            }
-            .tag(0)
-            NavigationView {
-                ActivityProgressView()
-            }
-            .tabItem {
-                if tabSelection == 1 { Image("chart.bar.fill") }
-                else { Image("chart.bar") }
-                Text("Progres")
-            }
-            .tag(1)
-            NavigationView {
-                AddNewRecipe()
-            }
-            .tabItem {
-                if tabSelection == 2 { Image("book.fill") }
-                else { Image("book") }
-                Text("Retete")
-            }
-            .tag(2)
-            NavigationView {
-                FitnessView()
-            }
-            .tabItem {
-                if tabSelection == 3 { Image("dumbbell.fill").imageScale(.large) }
-                else { Image("dumbbell").imageScale(.large) }
-                Text("Sport")
-            }
-            .tag(3)
-            NavigationView {
-                ForumView()
-            }
-            .tabItem {
-                if tabSelection == 4 { Image("person.2.fill") }
-                else { Image("person.2") }
-                Text("Comunitate")
-            }
-            .tag(4)
+            HomeView()
+                .tabItem {
+                    if tabSelection == 0 { Image("house.fill") }
+                    else { Image("house") }
+                    Text("Meniu")
+                }
+                .tag(0)
+            
+            ActivityProgressView()
+                .tabItem {
+                    if tabSelection == 1 { Image("chart.bar.fill") }
+                    else { Image("chart.bar") }
+                    Text("Progres")
+                }
+                .tag(1)
+            
+            RecipesView()
+                .tabItem {
+                    if tabSelection == 2 { Image("book.fill") }
+                    else { Image("book") }
+                    Text("Retete")
+                }
+                .tag(2)
+            FitnessView()
+                .tabItem {
+                    if tabSelection == 3 { Image("dumbbell.fill").imageScale(.large) }
+                    else { Image("dumbbell").imageScale(.large) }
+                    Text("Sport")
+                }
+                .tag(3)
+            
+            ForumView()
+                .tabItem {
+                    if tabSelection == 4 { Image("person.2.fill") }
+                    else { Image("person.2") }
+                    Text("Comunitate")
+                }
+                .tag(4)
         }
-        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
@@ -71,13 +68,14 @@ struct TabBar: View {
 struct TabBar_Previews: PreviewProvider {
     static var previews: some View {
         let viewModel = AuthViewModel()
-        viewModel.currentUser = User(fullname: "George Popescu", name: "popescu.george", email: "george.popescu@yahoo.com", is_staff: false)
-        return
-            Group {
-                TabBar()
-                TabBar()
-                    .preferredColorScheme(.dark)
-            }
-            .environmentObject(viewModel)
+        viewModel.currentUser = userPreviewData
+        let recipeViewModel = RecipeViewModel()
+        recipeViewModel.recipes = recipePreviewData
+        return Group {
+            TabBar()
+            TabBar()
+                .preferredColorScheme(.dark)
+        }
+        .environmentObject(viewModel)
     }
 }
