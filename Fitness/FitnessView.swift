4df847b6c010d63e923b7f74f957177ce3e7e875
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-05 19:07:00 +0300

    added navigationView from tabView to view

--- Fitness/FitnessView.swift
+++ Fitness/FitnessView.swift
@@ -13,39 +13,42 @@ struct FitnessView: View {
     
     // MARK: - BODY
     var body: some View {
-        ZStack {
-            Color.background
-                .edgesIgnoringSafeArea(.all)
-            
-            ScrollView(showsIndicators: false) {
-                VStack {
-                    HStack(alignment: .center) {
-                        SearchBar(placeholderText: "Cauta un exercitiu",text: .constant("")) // de adaugat bindingul corespunzator pentru functia de cautare
-                            .padding(.bottom, 3)
+        NavigationView {
+            ZStack {
+                Color.background
+                    .edgesIgnoringSafeArea(.all)
+                
+                ScrollView(showsIndicators: false) {
+                    VStack {
+                        HStack(alignment: .center) {
+                            SearchBar(placeholderText: "Cauta un exercitiu",text: .constant("")) // de adaugat bindingul corespunzator pentru functia de cautare
+                                .padding(.bottom, 3)
+                            
+                            Button {
+                                print("buton setari retete apasat")
+                            } label: {
+                                Image(systemName: "slider.horizontal.3")
+                                    .imageScale(.large)
+                            }
+                        }
+                        .padding([.top, .leading, .trailing])
+                        .padding(.bottom, 5.0)
+                        .frame(maxWidth: 580)
                         
-                        Button {
-                            print("buton setari retete apasat")
-                        } label: {
-                            Image(systemName: "slider.horizontal.3")
-                                .imageScale(.large)
+                        ForEach(excersises) { excersise in
+                            NavigationLink(destination: FitnessDetailView()) {
+                                FitnessCard(exercitiu: excersise)
+                                    .frame(maxWidth: 612)
+                                    .foregroundColor(.primary)
+                            }
                         }
-                    }
-                    .padding([.top, .leading, .trailing])
-                    .padding(.bottom, 5.0)
-                    .frame(maxWidth: 580)
-                    
-                    ForEach(excersises) { excersise in
-                        NavigationLink(destination: FitnessDetailView()) {
-                            FitnessCard(exercitiu: excersise)
-                                .frame(maxWidth: 612)
-                                .foregroundColor(.primary)
-                        }
-                    }
-                } //: END MAIN VSTACK
-            } //: END SCROLL VIEW
-            .navigationTitle("Fitness")
-            .navigationBarTitleDisplayMode(.inline)
+                    } //: END MAIN VSTACK
+                } //: END SCROLL VIEW
+                .navigationTitle("Fitness")
+                .navigationBarTitleDisplayMode(.automatic)
+            }
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
@@ -54,15 +57,9 @@ struct FitnessView: View {
 struct FitnessView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            NavigationView {
-                FitnessView()
-            }
-            .navigationViewStyle(StackNavigationViewStyle())
-            NavigationView {
-                FitnessView()
-                    .preferredColorScheme(.dark)
-            }
-            .navigationViewStyle(StackNavigationViewStyle())
+            FitnessView()
+            FitnessView()
+                .preferredColorScheme(.dark)
         }
     }
 }
