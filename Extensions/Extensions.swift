4533c8fba36a73054bd109f0a9abc52d23ab3ec1
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-06 16:19:54 +0300

    added extensions to override tab bar and navigation bar colours

--- Extensions/Extensions.swift
+++ Extensions/Extensions.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftUI
 
+
 extension Color {
     static let accent = Color("AccentColor")
     static let button = Color("ButtonColor")
@@ -16,4 +17,57 @@ extension Color {
     static let post = Color("PostDetailColor")
     static let accentLight = Color("AccentLight")
     static let formText = Color("SectionText")
+    static let mono = Color("BlackAndWhite")
+}
+
+// Override NavigationBar Color
+extension UINavigationController {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        let standard = UINavigationBarAppearance()
+        standard.backgroundColor = UIColor(Color.background)
+        standard.shadowColor = UIColor(Color.background)
+        navigationBar.standardAppearance = standard
+        navigationBar.compactAppearance = standard
+        navigationBar.scrollEdgeAppearance = standard
+ }
+}
+
+// Override TabBar Color
+extension View {
+  func tabViewStyle(backgroundColor: UIColor? = nil,
+                    itemColor: UIColor? = nil,
+                    selectedItemColor: UIColor? = nil,
+                    badgeColor: UIColor? = nil) -> some View {
+    onAppear {
+      let itemAppearance = UITabBarItemAppearance()
+      if let uiItemColor = itemColor {
+        itemAppearance.normal.iconColor = uiItemColor
+        itemAppearance.normal.titleTextAttributes = [
+          .foregroundColor: uiItemColor
+        ]
+      }
+      if let uiSelectedItemColor = selectedItemColor {
+        itemAppearance.selected.iconColor = uiSelectedItemColor
+        itemAppearance.selected.titleTextAttributes = [
+          .foregroundColor: uiSelectedItemColor
+        ]
+      }
+      if let uiBadgeColor = badgeColor {
+        itemAppearance.normal.badgeBackgroundColor = uiBadgeColor
+        itemAppearance.selected.badgeBackgroundColor = uiBadgeColor
+      }
+
+      let appearance = UITabBarAppearance()
+      if let uiBackgroundColor = backgroundColor {
+        appearance.backgroundColor = uiBackgroundColor
+      }
+
+      appearance.stackedLayoutAppearance = itemAppearance
+      appearance.inlineLayoutAppearance = itemAppearance
+      appearance.compactInlineLayoutAppearance = itemAppearance
+
+      UITabBar.appearance().standardAppearance = appearance
+    }
+  }
 }
