9b0d98eb385e7cd43ad2f56b01eff98cf5f147aa
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-08 15:47:26 +0300

    added adaptive button resizing

--- Home/AddDishButton.swift
+++ Home/AddDishButton.swift
@@ -12,6 +12,7 @@ struct AddDishButton: View {
     // MARK: - PROPERTIES
     var imageName: String
     var title: String
+    private var idiom: UIUserInterfaceIdiom { UIDevice.current.userInterfaceIdiom }
     
     init(imageName: String, title: String) {
         self.imageName = imageName
@@ -23,13 +24,14 @@ struct AddDishButton: View {
         VStack {
             Circle()
                 .stroke(Color.button, style: StrokeStyle(lineWidth: 5))
-                .frame(width: 48, height: 48)
+                .frame(width: idiom == .pad ? 48 : getScreenBounds().width * 0.12, height: idiom == .pad ? 48 : getScreenBounds().width * 0.12)
                 .foregroundColor(Color(UIColor.label))
                 .background(Circle().fill(Color.button))
                 .clipShape(Circle())
                 .overlay(
                     Image(imageName)
                         .font(Font.title.weight(.light))
+                        .frame(width: idiom == .pad ? 48 : getScreenBounds().width * 0.12, height: idiom == .pad ? 48 : getScreenBounds().width * 0.12)
                 )
             Text(title)
                 .font(.subheadline)
