acdab3726b3895b4d8e660ed442a9e1e03115f8b
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-12 10:48:50 +0300

    added async register function

--- ViewModel/AuthViewModel.swift
+++ ViewModel/AuthViewModel.swift
@@ -12,6 +12,7 @@ class AuthViewModel: ObservableObject {
     // MARK: - PROPERTIES
     @Published var currentUser: User?
     @Published var isLoggedIn = false
+    @Published var error: Error?
     
     private var keychainService = KeychainService()
     private let service = UserService()
@@ -94,6 +95,101 @@ class AuthViewModel: ObservableObject {
         
         DispatchQueue.main.async {
             self.isLoggedIn = false
+        }
+    }
+    
+    @MainActor
+    func signUp(email: String, username: String, fullname: String, password: String) async throws {
+        guard let url = URL(string: "http://127.0.0.1:8000/api/user/create/") else {
+            throw UserError.invalidURL
+        }
+        
+        // Add the payload to the http request data
+        let payload = [
+            "email": email,
+            "name": username,
+            "fullname": fullname,
+            "password": password
+        ] as [String: Any]
+
+        // REQUEST PROPERTIES
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: payload, options: .prettyPrinted)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        // Send a POST request to the URL, with the data we created earlier
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            guard (response as? HTTPURLResponse)?.statusCode == 201 else {
+                throw UserError.serverError
+            }
+            
+            if try JSONSerialization.jsonObject(with: data, options: .mutableContainers) is [String: Any] {}
+            
+            guard let user = try? JSONDecoder().decode(User.self, from: data) else {
+                throw UserError.invalidData
+            }
+            
+            try await getToken(email: email , password: password)
+
+            self.isLoggedIn = true
+            self.currentUser = user
+            
+        } catch {
+            self.error = error
+        }
+    }
+    
+    func getToken(email: String, password: String) async throws {
+        guard let url = URL(string: "http://127.0.0.1:8000/api/user/token/") else {
+            throw UserError.invalidURL
+        }
+        
+        // Add the payload to the http request data
+        let payload = [
+            "email": email,
+            "password": password
+        ] as [String: Any]
+        
+        // Add request properties
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: payload, options: .prettyPrinted)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        // Send a POST request to the URL, with the data we created earlier
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                throw UserError.serverError
+            }
+            
+            if try JSONSerialization.jsonObject(with: data, options: .mutableContainers) is [String: Any] {}
+            
+            guard let tokenObject = try? JSONDecoder().decode(Token.self, from: data) else {
+                throw UserError.invalidData
+            }
+            // Save user data
+            UserDefaults.standard.set(String(email), forKey: "userEmail")
+            try self.keychainService.save(service: "mise-foodtracker", account: email, password: tokenObject.token.data(using: .utf8) ?? Data())
+
+        } catch {
+            self.error = error
         }
     }
 }
