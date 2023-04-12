69562f60133e45bf79be4b08b2e236afd35d3dd7
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-12 10:48:15 +0300

    added user error model

--- Model/User.swift
+++ Model/User.swift
@@ -7,6 +7,26 @@
 
 import SwiftUI
 
+enum UserError: Error, LocalizedError {
+    case invalidURL
+    case serverError
+    case invalidData
+    case unknown(Error)
+    
+    var errorDescription: String? {
+        switch self {
+            case .invalidURL:
+                return ""
+            case .serverError:
+                return "S-a produs o eroare la partea de server. Va rugam incercati mai tarziu"
+            case .invalidData:
+                return "A aparut o eroare. Va rugam incercati mai tarziu."
+            case .unknown(let error):
+                return error.localizedDescription
+        }
+    }
+}
+
 // TOKEN MODEL
 struct Token: Codable {
     let token: String
@@ -26,3 +46,4 @@ extension User {
         User(fullname: "John Doe", name: "john.doe", email: "john.doe@email.com", is_staff: false)
     ]
 }
+
