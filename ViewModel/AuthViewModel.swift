67a73060be65d66898d561de80efd23e32ba48f1
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-13 22:52:12 +0300

    remade fetch user function

--- ViewModel/AuthViewModel.swift
+++ ViewModel/AuthViewModel.swift
@@ -19,84 +19,60 @@ class AuthViewModel: ObservableObject {
     
     init() {
         // CHECK IF USER IS ALREADY LOGGED IN
-        self.fetchUser()
+        self.taskGetUser()
     }
     
     static let shared = AuthViewModel()
     
     // MARK: - METHODS
-    func register(email: String, username: String, fullname: String, password: String) {
-        AuthServices.register(email: email, username: username, fullname: fullname, password: password) { result in
-            switch result {
-                case .success(let data):
-                    guard (try? JSONDecoder().decode(User.self, from: data!)) != nil else { return }
-                    DispatchQueue.main.async {
-                        self.fetchUser()
-                    }
-                case .failure(let error):
-                    print(error.localizedDescription)
+    @MainActor
+    func signIn(email: String, password: String) async throws {
+        guard let url = URL(string: "http://127.0.0.1:8000/api/user/token/") else {
+            throw UserError.invalidURL
+        }
+        
+        // Add the payload to the http request data
+        let payload = [
+            "email": email,
+            "password": password
+        ] as [String: Any]
+        
+        // Add request properties
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: payload, options: .prettyPrinted)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        // Send a POST request to the URL, with the data we created earlier
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                throw UserError.serverError
             }
+            
+            if try JSONSerialization.jsonObject(with: data, options: .mutableContainers) is [String: Any] {}
+            
+            guard let tokenObject = try? JSONDecoder().decode(Token.self, from: data) else {
+                throw UserError.invalidData
+            }
+            // Save user data
+            UserDefaults.standard.set(String(email), forKey: "userEmail")
+            try self.keychainService.save(service: "mise-foodtracker", account: email, password: tokenObject.token.data(using: .utf8) ?? Data())
+            
+            try await getUser()
+            
+        } catch {
+            self.error = error
         }
     }
     
-    func login(email: String, password: String) {
-        AuthServices.login(email: email, password: password) { result in
-            switch result {
-                case .success(let data):
-                    guard let response = try? JSONDecoder().decode(Token.self, from: data!) else {
-                        print("AuthServices-Login: No response from API")
-                        return
-                    }
-                    do { // save token in keychain
-                        UserDefaults.standard.set(String(email), forKey: "userEmail")
-                        try self.keychainService.save(service: "mise-foodtracker", account: email, password: response.token.data(using: .utf8) ?? Data())
-                    } catch {
-                        print("KeychainServiceError:\(error)")
-                    }
-                    DispatchQueue.main.async {
-                        self.isLoggedIn = true
-                        self.fetchUser()
-                    }
-                case .failure(let error):
-                    print(error.localizedDescription)
-            }
-        }
-    }
-    
-    func fetchUser() {
-        guard let email = UserDefaults.standard.string(forKey: "userEmail") else { return }
-        
-        service.fetchUser(email: email) { result in
-            switch result {
-                case .success(let data):
-                    guard let user = try? JSONDecoder().decode(User.self, from: data!) else { return }
-                    print("fetchUser: Succes fetching user")
-                    DispatchQueue.main.async {
-                        UserDefaults.standard.set(user.email, forKey: "userEmail")
-                        self.isLoggedIn = true
-                        self.currentUser = user
-                    }
-                    
-                case .failure(let error):
-                    print("AuthViewModel.fetchUser: \(error.localizedDescription)")
-            }
-        }
-    }
-    
-    func signOut() {
-        guard let email = UserDefaults.standard.string(forKey: "userEmail") else { return }
-        
-        // delete from token from keychain
-        guard (try? self.keychainService.remove(service: "mise-foodtracker", account: email)) != nil else {
-            print("KeychainService: Failed to remove keychain data")
-            return
-        }
-        UserDefaults.standard.removeObject(forKey: "userEmail")
-        
-        DispatchQueue.main.async {
-            self.isLoggedIn = false
-        }
-    }
     
     @MainActor
     func signUp(email: String, username: String, fullname: String, password: String) async throws {
@@ -111,7 +87,7 @@ class AuthViewModel: ObservableObject {
             "fullname": fullname,
             "password": password
         ] as [String: Any]
-
+        
         // REQUEST PROPERTIES
         var request = URLRequest(url: url)
         
@@ -139,12 +115,27 @@ class AuthViewModel: ObservableObject {
             }
             
             try await getToken(email: email , password: password)
-
+            
             self.isLoggedIn = true
             self.currentUser = user
             
         } catch {
             self.error = error
+        }
+    }
+    
+    func signOut() {
+        guard let email = UserDefaults.standard.string(forKey: "userEmail") else { return }
+        
+        // delete from token from keychain
+        guard (try? self.keychainService.remove(service: "mise-foodtracker", account: email)) != nil else {
+            print("KeychainService: Failed to remove keychain data")
+            return
+        }
+        UserDefaults.standard.removeObject(forKey: "userEmail")
+        
+        DispatchQueue.main.async {
+            self.isLoggedIn = false
         }
     }
     
@@ -187,9 +178,64 @@ class AuthViewModel: ObservableObject {
             // Save user data
             UserDefaults.standard.set(String(email), forKey: "userEmail")
             try self.keychainService.save(service: "mise-foodtracker", account: email, password: tokenObject.token.data(using: .utf8) ?? Data())
-
+            
         } catch {
             self.error = error
+        }
+    }
+    
+    @MainActor
+    func getUser() async throws {
+        var token = ""
+        guard let email = UserDefaults.standard.string(forKey: "userEmail") else { return }
+        // Get token from keychain
+        do {
+            guard let keychainResult = (try self.keychainService.get(service: "mise-foodtracker", account: email)) else {
+                print("KeychainService: Failed to read token.")
+                return
+            }
+            token = String(decoding: keychainResult, as: UTF8.self)
+        } catch {
+            print("fetchUser:\(error)")
+        }
+        
+        // CHECK IF URL EXISTS
+        guard let url = URL(string: "http://127.0.0.1:8000/api/user/me/") else {
+            throw UserError.invalidURL
+        }
+        
+        // REQUEST PROPERTIES
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "GET"
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("Token \(token)", forHTTPHeaderField: "Authorization")
+        
+        // Send a GET request to the URL, with the data we created earlier
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                throw UserError.serverError
+            }
+            
+            if try JSONSerialization.jsonObject(with: data, options: .mutableContainers) is [String: Any] {}
+            
+            guard let user = try? JSONDecoder().decode(User.self, from: data) else {
+                throw UserError.invalidData
+            }
+            self.currentUser = user
+            self.isLoggedIn = true
+            
+        } catch {
+            self.error = error
+        }
+    }
+    
+    func taskGetUser() {
+        Task {
+            try await getUser()
         }
     }
 }
