a0daf096f66f428a414c4631ff6aa84ce02ed56d
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-13 22:51:49 +0300

    added delete recipe function

--- ViewModel/Recipe/RecipeDetailViewModel.swift
+++ ViewModel/Recipe/RecipeDetailViewModel.swift
@@ -65,4 +65,43 @@ class RecipeDetailViewModel: ObservableObject {
             self.error = error
         }
     }
+    
+    func deleteRecipe() async throws {
+        guard let user = AuthViewModel.shared.currentUser else { return }
+        guard let url = URL(string: "http://127.0.0.1:8000/api/recipe/recipes/\(self.recipeId)/") else {
+            throw RecipeError.invalidURL
+        }
+        
+        // Get token from KeyChain
+        var token = ""
+        
+        do {
+            guard let keychainResult = (try self.keychainService.get(service: "mise-foodtracker", account: user.email)) else {
+                print("KeychainService: Failed to read token.")
+                return
+            }
+            token = String(decoding: keychainResult, as: UTF8.self)
+        } catch {
+            print("fetchUser:\(error)")
+        }
+        
+        // Set the URLRequest parameters
+        let session = URLSession.shared
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "DELETE"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("Token \(token)", forHTTPHeaderField: "Authorization")
+        
+        // Send a GET request to the URL and parse the response
+        do {
+            let (_, response) = try await session.data(for: request)
+            guard (response as? HTTPURLResponse)?.statusCode == 204 else {
+                throw RecipeError.serverError
+            }
+        } catch {
+            self.error = error
+        }
+    }
 }
