e792f0a9e5ef62592ede07ea4baee44646a74c1f
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-16 17:51:47 +0300

    added navigation view elements

--- Fitness/ActivityDetailConfrim.swift
+++ Fitness/ActivityDetailConfrim.swift
@@ -19,122 +19,118 @@ struct ActivityDetailConfrim: View {
         ZStack {
             Color.background
                 .edgesIgnoringSafeArea(.all)
-            VStack(alignment: .center, spacing: 12) {
-                // HEADER
-                VStack {
-                    HStack {
-                        Text("Alergare usoară")
-                            .font(.title)
-                            .fontWeight(.bold)
-                        Spacer()
-                        Button {
-                            presentationMode.wrappedValue.dismiss()
-                        } label: {
-                            Image(systemName: "xmark.circle")
-                                .font(.title2)
-                        }
-                    }
-                }
-                .padding(.horizontal, 24)
-                .padding(.top, 40)
-                
-                // FOOD META
-                HStack {
-                    Text("Timp efectuat:")
-                        .foregroundColor(Color.formText)
-                    TextField("", text: $activityTime)
-                    Text("minute")
-                        .foregroundColor(Color.formText)
-                }
-                .card()
-                HStack {
-                    Text("Greutate:")
-                        .foregroundColor(Color.formText)
-                    TextField("", text: $userWeight)
-                    Text("kg")
-                        .foregroundColor(Color.formText)
-                }
-                .card()
-                
-                HStack(alignment: .center) {
-                    HStack(alignment: .center) {
-                        Image(systemName: "cylinder.split.1x2")
-                            .font(Font.largeTitle.weight(.regular))
-                            .imageScale(.large)
-                            .foregroundColor(Color.accent)
-                            
-                        VStack(alignment: .leading) {
-                            Text("Valoare MET")
-                                .font(.headline)
-                            Text("6.0")
-                                .font(.title2)
-                                .fontWeight(.semibold)
-                        }
-                        Spacer()
-                    }
-                    .padding()
-                    .background(Color.card)
-                    .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
-                    .padding(.leading)
-                    HStack(alignment: .center) {
-                        Image(systemName: "stopwatch")
-                            .font(Font.largeTitle.weight(.regular))
-                            .imageScale(.large)
-                            .foregroundColor(Color.accent)
-                        VStack(alignment: .leading) {
-                            Text("Timp MET")
-                                .font(.headline)
-                            Text("18.0")
-                                .font(.title2)
-                                .fontWeight(.semibold)
-                        }
-                        Spacer()
-                    }
-                    .padding()
-                    .background(Color.card)
-                    .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
-                    .padding(.trailing)
-                }
-                
-                HStack(alignment: .center) {
-                    HStack {
-                        Image(systemName: "flame")
-                            .font(Font.largeTitle.weight(.regular))
-                            .imageScale(.large)
-                            .foregroundColor(Color.accent)
-                        VStack(alignment: .leading) {
-                            Text("Calorii arse")
-                                .font(.title2)
-                            Text("246 kCal")
+            ScrollView(showsIndicators: false) {
+                VStack(alignment: .center, spacing: 12) {
+                    // HEADER
+                    VStack {
+                        HStack {
+                            Text("Alergare usoară")
                                 .font(.title)
-                                .fontWeight(.semibold)
+                                .fontWeight(.bold)
+                            Spacer()
                         }
                     }
-                    .frame(maxWidth: .infinity)
+                    .padding(.horizontal, 24)
+                    .padding(.top, 40)
                     
-                }
-                .card()
-                // FOOD CHART
-                
-                
-                // ADD TO JOURNAL
-                Button {
-                    // viewModel.login(withEmail: email, password: password)
-                } label: {
-                    Text("Adaugă la jurnal")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .frame(width: 340, height: 50)
-                        .background(Color.accent)
-                        .clipShape(Capsule())
+                    // FOOD META
+                    HStack {
+                        Text("Timp efectuat:")
+                            .foregroundColor(Color.formText)
+                        TextField("", text: $activityTime)
+                        Text("minute")
+                            .foregroundColor(Color.formText)
+                    }
+                    .card()
+                    HStack {
+                        Text("Greutate:")
+                            .foregroundColor(Color.formText)
+                        TextField("", text: $userWeight)
+                        Text("kg")
+                            .foregroundColor(Color.formText)
+                    }
+                    .card()
+                    
+                    HStack(alignment: .center) {
+                        HStack(alignment: .center) {
+                            Image(systemName: "cylinder.split.1x2")
+                                .font(Font.largeTitle.weight(.regular))
+                                .imageScale(.large)
+                                .foregroundColor(Color.accent)
+                                
+                            VStack(alignment: .leading) {
+                                Text("Valoare MET")
+                                    .font(.headline)
+                                Text("6.0")
+                                    .font(.title2)
+                                    .fontWeight(.semibold)
+                            }
+                            Spacer()
+                        }
                         .padding()
-                }
-                .shadow(color: Color.black.opacity(0.1), radius: 12)
-                
-                Spacer()
-            } //: END MAIN VSTACK
-            .frame(maxWidth: 580)
-            .navigationBarTitle("Detalii Servire", displayMode: .inline)
+                        .background(Color.card)
+                        .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
+                        .padding(.leading)
+                        HStack(alignment: .center) {
+                            Image(systemName: "stopwatch")
+                                .font(Font.largeTitle.weight(.regular))
+                                .imageScale(.large)
+                                .foregroundColor(Color.accent)
+                            VStack(alignment: .leading) {
+                                Text("Timp MET")
+                                    .font(.headline)
+                                Text("18.0")
+                                    .font(.title2)
+                                    .fontWeight(.semibold)
+                            }
+                            Spacer()
+                        }
+                        .padding()
+                        .background(Color.card)
+                        .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
+                        .padding(.trailing)
+                    }
+                    
+                    HStack(alignment: .center) {
+                        HStack {
+                            Image(systemName: "flame")
+                                .font(Font.largeTitle.weight(.regular))
+                                .imageScale(.large)
+                                .foregroundColor(Color.accent)
+                            VStack(alignment: .leading) {
+                                Text("Calorii arse")
+                                    .font(.title2)
+                                Text("246 kCal")
+                                    .font(.title)
+                                    .fontWeight(.semibold)
+                            }
+                        }
+                        .frame(maxWidth: .infinity)
+                        
+                    }
+                    .card()
+                    // FOOD CHART
+                    
+                    
+                    // ADD TO JOURNAL
+                    Button {
+                        // viewModel.login(withEmail: email, password: password)
+                    } label: {
+                        Text("Adaugă la jurnal")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(width: 340, height: 50)
+                            .background(Color.accent)
+                            .clipShape(Capsule())
+                            .padding()
+                    }
+                    .shadow(color: Color.black.opacity(0.1), radius: 12)
+                    
+                    Spacer()
+                } //: END MAIN VSTACK
+                .frame(maxWidth: 580)
+            .navigationBarTitle("", displayMode: .inline)
+            }
         } //: END ZSTACK
     }
 }
@@ -142,9 +138,13 @@ struct ActivityDetailConfrim: View {
 struct ActivityDetailConfrim_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ActivityDetailConfrim()
-            ActivityDetailConfrim()
-                .preferredColorScheme(.dark)
+            NavigationView {
+                ActivityDetailConfrim()
+            }
+            NavigationView {
+                ActivityDetailConfrim()
+            }
+            .preferredColorScheme(.dark)
         }
     }
 }
