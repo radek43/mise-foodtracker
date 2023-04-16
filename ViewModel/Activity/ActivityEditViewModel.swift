fde0f3ff992076459f6600c5dbaa5101e60b5819
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-16 19:10:41 +0300

    added view model for edit activity view

---
+++ ViewModel/Activity/ActivityEditViewModel.swift
@@ -0,0 +1,76 @@
+//
+//  EditActivityViewModel.swift
+//  mise-foodtracker
+//
+//  Created by Radu Bila on 16.04.2023.
+//
+
+import Foundation
+import SwiftUI
+
+
+class ActivityEditViewModel: ObservableObject {
+    // MARK: - PROPERTIES
+    @Published var didUpdateActivity = false
+    @Published var error: Error?
+    
+    private var keychainService = KeychainService()
+    
+    // MARK: - METHODS
+    @MainActor
+    func updateActivity(id: Int, title: String, met: String) async throws {
+        guard let user = AuthViewModel.shared.currentUser else { return }
+        guard let url = URL(string: "http://127.0.0.1:8000/api/activity/activities/\(id)/") else {
+            throw RecipeError.invalidURL
+        }
+        
+        // Add the payload to the http request data
+        let params = [
+            "id": id,
+            "title": title,
+            "met": met,
+        ] as [String : Any]
+        
+        var token = ""
+        
+        // Get token from KeyChain
+        do {
+            guard let keychainResult = (try self.keychainService.get(service: "mise-foodtracker", account: user.email)) else {
+                print("KeychainService: Failed to read token.")
+                return
+            }
+            token = String(decoding: keychainResult, as: UTF8.self)
+        } catch {
+            print("fetchUser:\(error)")
+        }
+        
+        // Set the URLRequest to POST and to the specified URL
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "PUT"
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("Token \(token)", forHTTPHeaderField: "Authorization")
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
+        } catch let error {
+            print("postRecipe: \(error)")
+        }
+        
+        // Send a POST request to the URL, with the data we created earlier
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                throw RecipeError.serverError
+            }
+            if try JSONSerialization.jsonObject(with: data, options: [.mutableContainers, .fragmentsAllowed]) is [String: Any] {
+                
+            }
+            self.didUpdateActivity = true
+        } catch {
+            self.error = error
+        }
+    }
+}
