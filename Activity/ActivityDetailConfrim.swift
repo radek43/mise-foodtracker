1b191a9f91771fd93d4cd1ee824eff26c1bcb211
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-25 22:04:19 +0300

    code cleanup

--- Activity/ActivityDetailConfrim.swift
+++ Activity/ActivityDetailConfrim.swift
@@ -22,161 +22,152 @@ struct ActivityDetailConfrim: View {
     
     // MARK: - BODY
     var body: some View {
-        if authViewModel.currentUser != nil {
-            ZStack(alignment: .top) {
-                Color.background
-                    .edgesIgnoringSafeArea(.all)
-                if let activityData = activityDetailViewModel.activity {
-                    ScrollView(showsIndicators: false) {
-                        VStack(alignment: .leading, spacing: 12) {
-                            // HEADER
-                            Text(activityData.title)
-                                .font(.title)
-                                .fontWeight(.bold)
-                                .padding([.horizontal, .top], 24)
-                            
-                            // FOOD META
-                            HStack {
-                                Text("Timp efectuat:")
-                                    .foregroundColor(Color.formText)
-                                TextField("", text: $activityTime)
-                                Text("minute")
-                                    .foregroundColor(Color.formText)
-                            }
-                            .card()
-                            HStack {
-                                Text("Greutate:")
-                                    .foregroundColor(Color.formText)
-                                TextField("", text: $userWeight)
-                                Text("kg")
-                                    .foregroundColor(Color.formText)
-                            }
-                            .card()
-                            
+        ZStack(alignment: .top) {
+            Color.background
+                .edgesIgnoringSafeArea(.all)
+            if let activityData = activityDetailViewModel.activity {
+                ScrollView(showsIndicators: false) {
+                    VStack(alignment: .leading, spacing: 12) {
+                        Text(activityData.title)
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding([.horizontal, .top], 24)
+                        
+                        HStack {
+                            Text("Timp efectuat:")
+                                .foregroundColor(Color.formText)
+                            TextField("", text: $activityTime)
+                            Text("minute")
+                                .foregroundColor(Color.formText)
+                        }
+                        .card()
+                        HStack {
+                            Text("Greutate:")
+                                .foregroundColor(Color.formText)
+                            TextField("", text: $userWeight)
+                            Text("kg")
+                                .foregroundColor(Color.formText)
+                        }
+                        .card()
+                        
+                        HStack(alignment: .center) {
                             HStack(alignment: .center) {
-                                HStack(alignment: .center) {
-                                    Image(systemName: "cylinder.split.1x2")
-                                        .font(Font.largeTitle.weight(.regular))
-                                        .imageScale(.large)
-                                        .foregroundColor(Color.accent)
-                                        
-                                    VStack(alignment: .leading) {
-                                        Text("Valoare MET")
-                                            .font(.headline)
-                                        Text(activityData.met)
-                                            .font(.title2)
-                                            .fontWeight(.semibold)
-                                    }
-                                    Spacer()
+                                Image(systemName: "cylinder.split.1x2")
+                                    .font(Font.largeTitle.weight(.regular))
+                                    .imageScale(.large)
+                                    .foregroundColor(Color.accent)
+                                
+                                VStack(alignment: .leading) {
+                                    Text("Valoare MET")
+                                        .font(.headline)
+                                    Text(activityData.met)
+                                        .font(.title2)
+                                        .fontWeight(.semibold)
                                 }
-                                .padding()
-                                .background(Color.card)
-                                .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
-                                .padding(.leading)
-                                HStack(alignment: .center) {
-                                    Image(systemName: "stopwatch")
-                                        .font(Font.largeTitle.weight(.regular))
-                                        .imageScale(.large)
-                                        .foregroundColor(Color.accent)
-                                    VStack(alignment: .leading) {
-                                        Text("Timp MET")
-                                            .font(.headline)
-                                        Text(activityData.met)
-                                            .font(.title2)
-                                            .fontWeight(.semibold)
-                                    }
-                                    Spacer()
-                                }
-                                .padding()
-                                .background(Color.card)
-                                .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
-                                .padding(.trailing)
+                                Spacer()
                             }
-                            
+                            .padding()
+                            .background(Color.card)
+                            .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
+                            .padding(.leading)
                             HStack(alignment: .center) {
-                                HStack {
-                                    Image(systemName: "flame")
-                                        .font(Font.largeTitle.weight(.regular))
-                                        .imageScale(.large)
-                                        .foregroundColor(Color.accent)
-                                    VStack(alignment: .leading) {
-                                        Text("Calorii arse")
-                                            .font(.title2)
-                                        Text("246 kCal")
-                                            .font(.title)
-                                            .fontWeight(.semibold)
-                                    }
+                                Image(systemName: "stopwatch")
+                                    .font(Font.largeTitle.weight(.regular))
+                                    .imageScale(.large)
+                                    .foregroundColor(Color.accent)
+                                VStack(alignment: .leading) {
+                                    Text("Timp MET")
+                                        .font(.headline)
+                                    Text(activityData.met)
+                                        .font(.title2)
+                                        .fontWeight(.semibold)
                                 }
-                                .frame(maxWidth: .infinity)
+                                Spacer()
                             }
-                            .card()
-                            // FOOD CHART
-                            
-                            
-                            // ADD TO JOURNAL
-                            Button {
-                                // viewModel.login(withEmail: email, password: password)
-                            } label: {
-                                Text("Adaugă la jurnal")
-                                    .font(.headline)
-                                    .foregroundColor(.white)
-                                    .frame(height: 50)
-                                    .frame(maxWidth: 580)
-                                    .background(Color.accent)
-                                    .clipShape(Capsule())
-                                    .padding()
-                            }
-                            .shadow(color: Color.black.opacity(0.1), radius: 12)
-                            
-                            Spacer()
-                        } //: END MAIN VSTACK
-                        .frame(maxWidth: 580)
-                        .navigationBarItems(trailing: authViewModel.currentUser?.is_staff == true ?
-                            HStack(spacing: 15) {
-                                NavigationLink {
-                                    EditActivityView(activity: activityData)
-                                } label: {
-                                    HStack {
-                                        Text("Editează")
-                                        Image(systemName: "pencil.circle")
-                                    }
-                                }
-                                Button {
-                                     showDeleteConfirmation.toggle()
-                                } label: {
-                                    HStack {
-                                        Text("Șterge")
-                                            .foregroundColor(Color.red)
-                                        Image(systemName: "trash.circle")
-                                            .foregroundColor(Color.red)
-                                    }
-                                }
-                                .alert(isPresented:$showDeleteConfirmation) {
-                                    Alert(
-                                        title: Text("Ești sigur că vrei să ștergi această activitate?"),
-                                        message: Text("Această acțiune este permanentă"),
-                                        primaryButton: .destructive(Text("Șterge")) {
-                                            Task {
-                                                try await activityDetailViewModel.deleteActivity()
-                                            }
-                                            self.presentationMode.wrappedValue.dismiss()
-                                        },
-                                        secondaryButton: .cancel(Text("Renunță"))
-                                    )
+                            .padding()
+                            .background(Color.card)
+                            .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
+                            .padding(.trailing)
+                        }
+                        
+                        HStack(alignment: .center) {
+                            HStack {
+                                Image(systemName: "flame")
+                                    .font(Font.largeTitle.weight(.regular))
+                                    .imageScale(.large)
+                                    .foregroundColor(Color.accent)
+                                VStack(alignment: .leading) {
+                                    Text("Calorii arse")
+                                        .font(.title2)
+                                    Text("246 kCal")
+                                        .font(.title)
+                                        .fontWeight(.semibold)
                                 }
                             }
-                            : nil
-                        )
-                        .navigationBarTitle("", displayMode: .inline)
+                            .frame(maxWidth: .infinity)
+                        }
+                        .card()
+                        
+                        
+                        Button {
+                            // addToJournal()
+                        } label: {
+                            Text("Adaugă la jurnal")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .frame(height: 50)
+                                .frame(maxWidth: 580)
+                                .background(Color.accent)
+                                .clipShape(Capsule())
+                                .padding()
+                        }
                     }
+                    .frame(maxWidth: 580)
+                    .navigationBarItems(trailing: authViewModel.currentUser?.is_staff == true ?
+                        HStack(spacing: 15) {
+                            NavigationLink {
+                                EditActivityView(activity: activityData)
+                            } label: {
+                                HStack {
+                                    Text("Editează")
+                                    Image(systemName: "pencil.circle")
+                                }
+                            }
+                            Button {
+                                showDeleteConfirmation.toggle()
+                            } label: {
+                                HStack {
+                                    Text("Șterge")
+                                        .foregroundColor(Color.red)
+                                    Image(systemName: "trash.circle")
+                                        .foregroundColor(Color.red)
+                                }
+                            }
+                            .alert(isPresented:$showDeleteConfirmation) {
+                                Alert(
+                                    title: Text("Ești sigur că vrei să ștergi această activitate?"),
+                                    message: Text("Această acțiune este permanentă"),
+                                    primaryButton: .destructive(Text("Șterge")) {
+                                        Task {
+                                            try await activityDetailViewModel.deleteActivity()
+                                        }
+                                        self.presentationMode.wrappedValue.dismiss()
+                                    },
+                                    secondaryButton: .cancel(Text("Renunță"))
+                                )
+                            }
+                        }
+                        : nil
+                    )
+                    .navigationBarTitle("", displayMode: .inline)
                 }
             }
-            .onAppear {
-                Task(priority: .medium) {
-                    try await self.activityDetailViewModel.fetchActivity()
-                }
+        }
+        .onAppear {
+            Task(priority: .medium) {
+                try await self.activityDetailViewModel.fetchActivity()
             }
-        } //: END ZSTACK
+        }
     }
 }
 
@@ -185,7 +176,7 @@ struct ActivityDetailConfrim_Previews: PreviewProvider {
         let authViewModel = AuthViewModel()
         authViewModel.currentUser = userPreviewData
         let viewModel = ActivityDetailViewModel(withActivityId: 1)
-        viewModel.activity = previewActivityData[0]
+        viewModel.activity = activityPreviewData[0]
         return Group {
             NavigationView {
                 ActivityDetailConfrim(activityDetailViewModel: viewModel)
