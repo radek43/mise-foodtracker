2dfbfa2e4cc56ee19767f118e5d92bbd6e9da7c5
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-25 22:04:06 +0300

    code cleanup

--- Activity/ActivityInfoView.swift
+++ Activity/ActivityInfoView.swift
@@ -16,7 +16,7 @@ struct ActivityInfoView: View {
         ZStack {
             Color.background
                 .edgesIgnoringSafeArea(.all)
-            VStack(alignment: .leading) {
+            VStack(alignment: .center) {
                 HStack {
                     Text("Ce este MET?")
                         .font(.title)
@@ -26,7 +26,6 @@ struct ActivityInfoView: View {
                         presentationMode.wrappedValue.dismiss()
                     } label: {
                         Image(systemName: "xmark.circle")
-                            .font(.title2)
                     }
                 }
                 .padding(.horizontal, 24)
@@ -34,14 +33,20 @@ struct ActivityInfoView: View {
                 
                 VStack {
                     Text("„Echivalentul metabolic” (notat MET) e o masura a consumului energetic ce ne permite sa comparam activitati diferite intre persoane diferite. MET-ul e un multiplu al Ratei Metabolice de Baza per ora.")
+                        .font(.body)
+                        .fontWeight(.regular)
                         .card()
                     Text("MET reprezinta caloriile arse prin rata metabolica de baza, iar toate activitatile sunt un multiplu al acesteia. Somnul e singurul ce consuma mai putin (0.9 MET) iar pe masura ce intensitatea creste se poate ajunge chiar si la 23 MET.")
+                        .font(.body)
+                        .fontWeight(.regular)
                         .card()
                     Text("Formula simpla: Caloriile arse de o activitate intr-o ora = MET specific activitatii x Greutatea ta corporala \r\n\nFormula mai precisa: Caloriile arse de o activitate intr-o ora = MET specific activitatii x Rata Metabolica de Baza / 24 \r\n\nPentru a calcula in minute, pur si simplu imparti rezultatul la 60 (de minute intr-o ora) si inmultesti cu numarul de minute.")
+                        .font(.body)
+                        .fontWeight(.regular)
                         .card()
                 }
+                Spacer()
             }
-            .frame(maxWidth: 580)
         }
     }
 }
