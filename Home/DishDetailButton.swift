b4864d9c94c18474fce836de016b09e6f200d7ef
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-25 22:01:49 +0300

    code cleanup

--- Home/DishDetailButton.swift
+++ Home/DishDetailButton.swift
@@ -2,7 +2,7 @@
 //  DishDetailButton.swift
 //  mise-foodtracker
 //
-//  Created by Radu Bila on 1/10/23.
+//  Created by Radu Bila on 10.01.2023.
 //
 
 import SwiftUI
@@ -13,6 +13,8 @@ struct DishDetailButton: View {
     var propertyTitle: String
     var propertyValue: String
     
+    private var idiom: UIUserInterfaceIdiom { UIDevice.current.userInterfaceIdiom }
+    
     init(imageName: String, propertyTitle: String, propertyValue: String) {
         self.imageName = imageName // assign all the parameters, not only `content`
         self.propertyTitle = propertyTitle
@@ -22,9 +24,10 @@ struct DishDetailButton: View {
     // MARK: - BODY
     var body: some View {
         VStack {
+            // Dish icon
             Circle()
                 .stroke(Color.button, style: StrokeStyle(lineWidth: 5))
-                .frame(width: 48, height: 48)
+                .frame(width: idiom == .pad ? 48 : getScreenBounds().width * 0.12, height: idiom == .pad ? 48 : getScreenBounds().width * 0.12)
                 .foregroundColor(Color(UIColor.label))
                 .background(Circle().fill(Color.button))
                 .clipShape(Circle())
@@ -32,6 +35,7 @@ struct DishDetailButton: View {
                     Image(imageName)
                         .font(Font.title.weight(.light))
                 )
+            // Dish properties
             Text(propertyTitle)
                 .font(.footnote)
                 .foregroundColor(Color.primary)
