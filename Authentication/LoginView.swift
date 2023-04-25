42ce209e02a00376f43aa4da2ca67d96cf83a5a5
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-25 21:57:54 +0300

    changed UI design

--- Authentication/LoginView.swift
+++ Authentication/LoginView.swift
@@ -2,7 +2,7 @@
 //  LoginView.swift
 //  mise-foodtracker
 //
-//  Created by Radu Bila on 8/9/22.
+//  Created by Radu Bila on 09.08.2022.
 //
 
 import SwiftUI
@@ -21,46 +21,38 @@ struct LoginView: View {
                 Color.background
                     .edgesIgnoringSafeArea(.all)
                 VStack {
-                    //  HEADER
+                    // Header card
                     AuthenticationHeaderView(title1: "Bine ai venit!", title2: "")
                     
-                    VStack(spacing: 40) {
+                    VStack(spacing: 18.0) {
                         CustomInputFields(imageName: "envelope", placeholderText: "Email", text: $email)
                         CustomInputFields(imageName: "lock", placeholderText: "Parola", isSecureField: true, text: $password)
                     }
-                    .padding(.horizontal, 32)
-                    .padding(.top, 44)
-                    //  RESET PASSWORD
+                    .padding(.top, 30)
+                    // Password reset
                     HStack {
                         Spacer()
                         NavigationLink {
                             Text("Reset password view..")
                         } label: {
-                            Text("Ai uitat parola?")
+                            Text("Am uitat parola")
                                 .font(.caption)
                                 .fontWeight(.semibold)
                                 .padding(.top)
                                 .padding(.trailing, 24)
                         }
                     }
-                    //  AUTHENTICATION
+                    // Sign in
                     Button {
                         Task {
                             try await authViewModel.signIn(email: email, password: password)
                         }
                     } label: {
-                        Text("Autentifica-te")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .frame(width: 340, height: 50)
-                            .background(Color.accent)
-                            .clipShape(Capsule())
-                            .padding()
+                        CapsuleButton(text: "Autentifică-te")
                     }
-                    .shadow(color: Color.black.opacity(0.1), radius: 12)
                     
                     Spacer()
-                    //  REGISTER
+                    // Register
                     NavigationLink {
                         RegistrationView()
                             .navigationBarHidden(true)
@@ -68,19 +60,17 @@ struct LoginView: View {
                         HStack {
                             Text("Nu ai cont?")
                                 .font(.footnote)
-                            Text("Inregistreaza-te")
+                            Text("Înregistrează-te")
                                 .font(.footnote)
                                 .fontWeight(.semibold)
                         }
                     }
-                    .padding(.bottom, 32)
                 }
-                .ignoresSafeArea()
+                .edgesIgnoringSafeArea(.top)
                 .navigationBarHidden(true)
                 .navigationViewStyle(StackNavigationViewStyle())
-            }  //:  END ZSTACK
-        }  //:  END NAVIGATION VIEW
-        .navigationViewStyle(StackNavigationViewStyle())
+            }
+        }
     }
 }
 
