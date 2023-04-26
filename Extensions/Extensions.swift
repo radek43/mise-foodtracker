47183f2f6b86f9b928265eb48f3a912d5428980c
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-26 18:17:28 +0300

    added extension for navigation bar

--- Extensions/Extensions.swift
+++ Extensions/Extensions.swift
@@ -100,3 +100,19 @@ extension Float {
         return (self * divisor).rounded() / divisor
     }
 }
+
+extension UINavigationBar {
+    static func changeAppearance(clear: Bool) {
+        let appearance = UINavigationBarAppearance()
+        
+        if clear {
+            appearance.configureWithTransparentBackground()
+        } else {
+            appearance.configureWithDefaultBackground()
+        }
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+}
