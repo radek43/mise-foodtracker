17e4812427b1b7db96a1e5e0c6090f0f952974b6
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-05-04 12:02:11 +0300

    added pull to refresh

--- Activity/ActivityView.swift
+++ Activity/ActivityView.swift
@@ -21,68 +21,82 @@ struct ActivityView: View {
     var body: some View {
         if let user = authViewModel.currentUser {
             NavigationView {
-                ScrollView(showsIndicators: false) {
-                    ZStack {
-                        Color.background.ignoresSafeArea(.all)
-                        
-                        // Activity list
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Text("Nume activitate")
-                                    .font(.headline)
-                                    .fontWeight(.bold)
-                                Spacer()
-                                Text("MET")
-                                    .font(.headline)
-                                    .fontWeight(.bold)
+                ZStack {
+                    Color.background.ignoresSafeArea(.all)
+                    
+                    ScrollView(showsIndicators: false) {
+                        // Refresh list
+                        RefreshControl(coordinateSpace: .named("RefreshControl")) {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                                activityViewModel.loadActivities()
                             }
-                            Divider()
-                            ForEach(activityViewModel.activities) { activity in
-                                NavigationLink {
-                                    ActivityDetailConfrim(activityDetailViewModel: ActivityDetailViewModel(withActivityId: activity.id))
-                                } label: {
-                                    HStack {
-                                        Text(activity.title)
-                                            .foregroundColor(Color(UIColor.label))
-                                            .fixedSize(horizontal: false, vertical: true)
-                                        Spacer()
-                                        Text(activity.met)
-                                            .foregroundColor(Color(UIColor.label))
+                        }
+                        VStack {
+                            // Search bar
+                            SearchBar(placeholderText: "Caută o activitate",text: .constant(""))
+                                .padding([.top, .horizontal])
+                                .padding(.bottom, 5.0)
+                            
+                            // Activity list
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Text("Nume activitate")
+                                        .font(.headline)
+                                        .fontWeight(.bold)
+                                    Spacer()
+                                    Text("MET")
+                                        .font(.headline)
+                                        .fontWeight(.bold)
+                                }
+                                Divider()
+                                ForEach(activityViewModel.activities) { activity in
+                                    NavigationLink {
+                                        ActivityDetailConfrim(activityDetailViewModel: ActivityDetailViewModel(withActivityId: activity.id))
+                                    } label: {
+                                        HStack {
+                                            Text(activity.title)
+                                                .foregroundColor(Color(UIColor.label))
+                                                .fixedSize(horizontal: false, vertical: true)
+                                            Spacer()
+                                            Text(activity.met)
+                                                .foregroundColor(Color(UIColor.label))
+                                        }
+                                    }
+                                    if activity.id != activityViewModel.activities.last?.id {
+                                        Divider()
                                     }
                                 }
-                                if activity.id != activityViewModel.activities.last?.id {
-                                    Divider()
+                            }
+                            .card()
+                            .padding(.bottom)
+                            .onAppear {
+                                Task {
+                                    try await self.activityViewModel.fetchActivities()
                                 }
                             }
+                            .navigationTitle("Activitate")
                         }
-                        .card()
-                        .padding(.bottom)
-                        .onAppear {
-                            Task {
-                                try await self.activityViewModel.fetchActivities()
+                    }
+                    .background(Color.background.edgesIgnoringSafeArea(.all))
+                    .coordinateSpace(name: "RefreshControl")
+                    .navigationBarItems(
+                        leading:
+                            Button {
+                                showInfoSheet.toggle()
+                            } label: {
+                                Image(systemName: "questionmark.circle")
                             }
-                        }
-                        .navigationTitle("Activitate")                    }
+                            .sheet(isPresented: $showInfoSheet) {
+                                ActivityInfoView()
+                            },
+                        trailing: user.is_staff == true ?
+                            NavigationLink {
+                                AddNewActivity()
+                            } label: {
+                                Image(systemName: "plus.circle")
+                            } : nil
+                    )
                 }
-                .background(Color.background.edgesIgnoringSafeArea(.all))
-                .navigationBarItems(
-                    leading:
-                        Button {
-                            showInfoSheet.toggle()
-                        } label: {
-                            Image(systemName: "questionmark.circle")
-                        }
-                        .sheet(isPresented: $showInfoSheet) {
-                            ActivityInfoView()
-                        },
-                    trailing: user.is_staff == true ?
-                        NavigationLink {
-                            AddNewActivity()
-                        } label: {
-                            Image(systemName: "plus.circle")
-                        }
-                    : nil
-                )
             }
             .navigationViewStyle(StackNavigationViewStyle())
         }
