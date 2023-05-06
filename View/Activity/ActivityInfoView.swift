82d6cda029bc6d2939e819a61f40dd7886700365
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-05-06 13:21:11 +0300

    added scroll view

--- View/Activity/ActivityInfoView.swift
+++ View/Activity/ActivityInfoView.swift
@@ -16,36 +16,38 @@ struct ActivityInfoView: View {
         ZStack {
             Color.background
                 .edgesIgnoringSafeArea(.all)
-            VStack(alignment: .center) {
-                HStack {
-                    Text("Ce este MET?")
-                        .font(.title)
-                        .fontWeight(.bold)
-                    Spacer()
-                    Button {
-                        presentationMode.wrappedValue.dismiss()
-                    } label: {
-                        Image(systemName: "xmark.circle")
+            ScrollView(showsIndicators: false) {
+                VStack(alignment: .center) {
+                    HStack {
+                        Text("Ce este MET?")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Spacer()
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                        } label: {
+                            Image(systemName: "xmark.circle")
+                        }
                     }
+                    .padding(.horizontal, 24)
+                    .padding(.top, 40)
+                    
+                    VStack {
+                        Text("„Echivalentul metabolic” (notat MET) e o masura a consumului energetic ce ne permite sa comparam activitati diferite intre persoane diferite. MET-ul e un multiplu al Ratei Metabolice de Baza per ora.")
+                            .font(.body)
+                            .fontWeight(.regular)
+                            .card()
+                        Text("MET reprezinta caloriile arse prin rata metabolica de baza, iar toate activitatile sunt un multiplu al acesteia. Somnul e singurul ce consuma mai putin (0.9 MET) iar pe masura ce intensitatea creste se poate ajunge chiar si la 23 MET.")
+                            .font(.body)
+                            .fontWeight(.regular)
+                            .card()
+                        Text("Formula simpla: Caloriile arse de o activitate intr-o ora = MET specific activitatii x Greutatea ta corporala \r\n\nFormula mai precisa: Caloriile arse de o activitate intr-o ora = MET specific activitatii x Rata Metabolica de Baza / 24 \r\n\nPentru a calcula in minute, pur si simplu imparti rezultatul la 60 (de minute intr-o ora) si inmultesti cu numarul de minute.")
+                            .font(.body)
+                            .fontWeight(.regular)
+                            .card()
+                    }
+                    Spacer()
                 }
-                .padding(.horizontal, 24)
-                .padding(.top, 40)
-                
-                VStack {
-                    Text("„Echivalentul metabolic” (notat MET) e o masura a consumului energetic ce ne permite sa comparam activitati diferite intre persoane diferite. MET-ul e un multiplu al Ratei Metabolice de Baza per ora.")
-                        .font(.body)
-                        .fontWeight(.regular)
-                        .card()
-                    Text("MET reprezinta caloriile arse prin rata metabolica de baza, iar toate activitatile sunt un multiplu al acesteia. Somnul e singurul ce consuma mai putin (0.9 MET) iar pe masura ce intensitatea creste se poate ajunge chiar si la 23 MET.")
-                        .font(.body)
-                        .fontWeight(.regular)
-                        .card()
-                    Text("Formula simpla: Caloriile arse de o activitate intr-o ora = MET specific activitatii x Greutatea ta corporala \r\n\nFormula mai precisa: Caloriile arse de o activitate intr-o ora = MET specific activitatii x Rata Metabolica de Baza / 24 \r\n\nPentru a calcula in minute, pur si simplu imparti rezultatul la 60 (de minute intr-o ora) si inmultesti cu numarul de minute.")
-                        .font(.body)
-                        .fontWeight(.regular)
-                        .card()
-                }
-                Spacer()
             }
         }
     }
