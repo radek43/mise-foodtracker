7dc82e1423cb1ce2186248594d90343f20f22cfe
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-05-14 08:52:10 +0300

    UI change and function integration

--- View/Home/DishConfirmView.swift
+++ View/Home/DishConfirmView.swift
@@ -19,72 +19,120 @@ struct DishConfirmView: View {
     
     @State private var data = Date()
     @State private var ammount = "100"
+    @State private var showDeleteConfirmation = false
+    
+    var dishType: Int
     
     // MARK: - BODY
     var body: some View {
         ZStack {
             Color.background
                 .edgesIgnoringSafeArea(.all)
-            if let dishData = dishConfirmViewModel.dish {
-                ScrollView(showsIndicators: false) {
-                    VStack(alignment: .leading) {
-                        // Header
-                        VStack {
-                            HStack {
-                                Text(dishData.title)
-                                    .font(.title)
-                                    .fontWeight(.semibold)
-                                Spacer()
+            if let user = authViewModel.currentUser {
+                if let dishData = dishConfirmViewModel.dish {
+                    ScrollView(showsIndicators: false) {
+                        VStack(alignment: .leading) {
+                            // Header
+                            VStack {
+                                HStack {
+                                    Text(dishData.title)
+                                        .font(.title)
+                                        .fontWeight(.semibold)
+                                    Spacer()
+                                }
                             }
-                        }
-                        .padding(.horizontal)
-                        .padding(.top)
-                        
-                        VStack(alignment: .center) {
-                            DatePicker(selection: $data, in: ...Date(), displayedComponents: .date) {
-                                Text("Data consumării:")
-                                    .foregroundColor(Color.formText)
-                            }
-                            .accentColor(.primary)
-                        }
-                        .card()
+                            .padding(.horizontal)
+                            .padding(.top)
+                            
+                            VStack(alignment: .center) {
+                                DatePicker(selection: $data, in: ...Date(), displayedComponents: .date) {
+                                    Text("Data consumării:")
+                                        .foregroundColor(Color.formText)
+                                }
+                                .onChange(of: data) { newDate in
+                                    if let midnight = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: newDate) {
+                                        data = midnight
 
-                        HStack {
-                            Text("Cantitate(g):")
-                                .foregroundColor(Color.formText)
-                            TextField("", text: $ammount)
-                                .numbersOnly($ammount, includeDecimal: true, decimalPlaces: 2, disableZero: true)
+                                    }
+                                }
+                                .accentColor(.primary)
+                            }
+                            .card()
+
+                            HStack {
+                                Text("Cantitate(g):")
+                                    .foregroundColor(Color.formText)
+                                TextField("", text: $ammount)
+                                    .numbersOnly($ammount, includeDecimal: true, decimalPlaces: 2, disableZero: true)
+                            }
+                            .card()
+                            
+                            if !dishData.estimates.isEmpty {
+                                Text("Estimări: \(dishData.estimates)")
+                                    .font(.footnote)
+                                    .foregroundColor(Color.formText)
+                                    .padding([.leading, .bottom, .trailing])
+                                    .padding(.leading, 8)
+                                    .padding(.top, -2)
+                            }
+                            
+                            NutritionChart(chartTitle: "Valori nutriționale",
+                                           protein: ammount.isEmpty ? 1 : calculateAmmount(grams: Double(ammount) ?? 0, nutrition: dishData.protein),
+                                           carbs: ammount.isEmpty ? 1 : calculateAmmount(grams: Double(ammount) ?? 0, nutrition: dishData.carbs),
+                                           fat: ammount.isEmpty ? 1 : calculateAmmount(grams: Double(ammount) ?? 0, nutrition: dishData.fat),
+                                           isDisabled: ammount.isEmpty ? true : false)
+                            
+                            // Add to journal
+                            Button {
+                                // viewModel.login(withEmail: email, password: password)
+                            } label: {
+                                RectangleButton(text: "Adaugă la jurnal")
+                            }
+                            
+                            Spacer()
                         }
-                        .card()
-                        
-                        if !dishData.estimates.isEmpty {
-                            Text("Estimări: \(dishData.estimates)")
-                                .font(.footnote)
-                                .foregroundColor(Color.formText)
-                                .padding([.leading, .bottom, .trailing])
-                                .padding(.leading, 8)
-                                .padding(.top, -2)
-                        }
-                        
-                        NutritionChart(chartTitle: "Valori nutriționale",
-                                       protein: ammount.isEmpty ? 1 : calculateAmmount(grams: Double(ammount) ?? 0, nutrition: dishData.protein),
-                                       carbs: ammount.isEmpty ? 1 : calculateAmmount(grams: Double(ammount) ?? 0, nutrition: dishData.carbs),
-                                       fat: ammount.isEmpty ? 1 : calculateAmmount(grams: Double(ammount) ?? 0, nutrition: dishData.fat),
-                                       isDisabled: ammount.isEmpty ? true : false)
-                        
-                        // Add to journal
-                        Button {
-                            // viewModel.login(withEmail: email, password: password)
-                        } label: {
-                            RectangleButton(text: "Adaugă la jurnal")
-                        }
-                        
-                        Spacer()
+                        .navigationBarItems(trailing: user.is_staff == true ?
+                            HStack(spacing: 15) {
+                                NavigationLink {
+                                    EditFoodView(food: dishData)
+                                } label: {
+                                    HStack {
+                                        Text("Editează")
+                                        Image(systemName: "highlighter")
+                                    }
+                                }
+                                Button {
+                                     showDeleteConfirmation.toggle()
+                                } label: {
+                                    HStack {
+                                        Text("Șterge")
+                                            .foregroundColor(Color.red)
+                                        Image(systemName: "trash")
+                                            .foregroundColor(Color.red)
+                                    }
+                                }
+                                .alert(isPresented:$showDeleteConfirmation) {
+                                    Alert(
+                                        title: Text("Ești sigur că vrei să ștergi această rețetă?"),
+                                        message: Text("Această acțiune este permanentă"),
+                                        primaryButton: .destructive(Text("Șterge")) {
+                                            Task {
+                                                try await dishConfirmViewModel.deleteDish()
+                                            }
+                                            self.presentationMode.wrappedValue.dismiss()
+                                        },
+                                        secondaryButton: .cancel(Text("Renunță"))
+                                    )
+                                }
+                            }
+                            : nil
+                        )
+                        .navigationBarTitle(user.is_staff == true ? "" : "Detalii Servire", displayMode: .inline)
                     }
-                    .navigationBarTitle("Detalii Servire", displayMode: .inline)
+                    .addHideKeyboardButton()
                 }
-                .addHideKeyboardButton()
             }
+
         }
         .onAppear {
             Task(priority: .medium) {
@@ -109,13 +157,14 @@ struct DishConfirmView_Previews: PreviewProvider {
         viewModel.dish = dishDetailPreviewData
         return Group {
             NavigationView {
-                DishConfirmView(dishConfirmViewModel: viewModel)
+                DishConfirmView(dishConfirmViewModel: viewModel, dishType: 1)
             }
             NavigationView {
-                DishConfirmView(dishConfirmViewModel: viewModel)
+                DishConfirmView(dishConfirmViewModel: viewModel, dishType: 1)
             }
             .preferredColorScheme(.dark)
         }
+        .environmentObject(authViewModel)
         .navigationViewStyle(StackNavigationViewStyle())
     }
 }
