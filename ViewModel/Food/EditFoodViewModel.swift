dbe7d13ef4af2987c6d936fef2f16f96bb48a042
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-05-14 08:50:13 +0300

    added viewmodel to edit foods in db

---
+++ ViewModel/Food/EditFoodViewModel.swift
@@ -0,0 +1,78 @@
+//
+//  EditFoodViewModel.swift
+//  mise-foodtracker
+//
+//  Created by Radu Bila on 11.05.2023.
+//
+
+import Foundation
+
+class EditFoodViewModel: ObservableObject {
+    // MARK: - PROPERTIES
+    @Published var didUpdateFood = false
+    @Published var error: Error?
+    
+    private var keychainService = KeychainService()
+    
+    // MARK: - METHODS
+    @MainActor
+    func updateFood(id: Int, title: String, calories: String, carbs: String, fibers: String, fat: String, protein: String, estimates: String) async throws {
+        guard let user = AuthViewModel.shared.currentUser else { return }
+        guard let url = URL(string: "http://127.0.0.1:8000/api/food/foods/\(id)/") else {
+            throw RecipeError.invalidURL
+        }
+        
+        // Add the payload to the request data
+        let params = [
+            "id": id,
+            "title": title,
+            "calories": calories,
+            "carbs": carbs,
+            "fibers": fibers,
+            "fat": fat,
+            "protein": protein,
+            "estimates": estimates,
+        ] as [String : Any]
+        
+        var token = ""
+        
+        // Get token from Keychain
+        do {
+            guard let keychainResult = (try self.keychainService.get(service: "mise-foodtracker", account: user.email)) else {
+                print("KeychainService: Failed to read token.")
+                return
+            }
+            token = String(decoding: keychainResult, as: UTF8.self)
+        } catch {
+            print("fetchUser:\(error)")
+        }
+        
+        // Set the URLRequest to PUT and to the specified URL
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "PUT"
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("Token \(token)", forHTTPHeaderField: "Authorization")
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
+        } catch let error {
+            print("postRecipe: \(error)")
+        }
+        
+        // Send a PUT request to the URL, with the data we created earlier
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                throw RecipeError.serverError
+            }
+            if try JSONSerialization.jsonObject(with: data, options: [.mutableContainers, .fragmentsAllowed]) is [String: Any] {
+            }
+            self.didUpdateFood = true
+        } catch {
+            self.error = error
+        }
+    }
+}
