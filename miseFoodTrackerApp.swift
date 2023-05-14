386926d2403d7a36578df4c652d964283207e1b6
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-05-14 08:53:21 +0300

    added log environment object to main function

--- miseFoodTrackerApp.swift
+++ miseFoodTrackerApp.swift
@@ -19,6 +19,7 @@ struct miseFoodTrackerApp: App {
         WindowGroup {
             LaunchView()
                 .environmentObject(AuthViewModel.shared)
+                .environmentObject(LogViewModel())
         }
     }
 }
