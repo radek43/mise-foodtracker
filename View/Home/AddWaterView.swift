bf2a3be0b365006c46d12a703221c96995438ebe
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-05-20 18:46:35 +0300

    added background

--- View/Home/AddWaterView.swift
+++ View/Home/AddWaterView.swift
@@ -25,67 +25,71 @@ struct AddWaterView: View {
     }
     
     var body: some View {
-        VStack {
-            Text("Apă consumată: \(percent, specifier: "%.0f")ml")
-            ZStack(alignment: .center) {
-                Rectangle()
-                    .fill(Color.secondary)
-                    .frame(width: 300, height: 320)
-                
-                Wave(offset: Angle(degrees: self.waveOffset.degrees), percent: Double(percent)/2000)
-                    .fill(Color(red: 0, green: 0.5, blue: 0.75, opacity: 0.5))
-                    .frame(width: 300, height: 320)
+        ZStack {
+            Color.background.ignoresSafeArea(.all)
+            VStack {
+                Text("Apă consumată: \(percent, specifier: "%.0f")ml")
+                ZStack(alignment: .center) {
+                    Rectangle()
+                        .fill(Color.secondary)
+                        .frame(width: 300, height: 320)
                     
-                Wave(offset: Angle(degrees: self.waveOffset2.degrees), percent: Double(percent)/2000)
-                    .fill(Color(red: 0, green: 0.5, blue: 0.75, opacity: 0.5))
-                    .opacity(0.5)
-                    .frame(width: 300, height: 320)
-            }
-            .mask(
-                Image("glass")
-                    .interpolation(.none)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 350, height: 350)
-            )
-            .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local).onEnded({ value in
-                if value.translation.height < 0 {
-                    percent += 250
+                    Wave(offset: Angle(degrees: self.waveOffset.degrees), percent: Double(percent)/2000)
+                        .fill(Color(red: 0, green: 0.5, blue: 0.75, opacity: 0.5))
+                        .frame(width: 300, height: 320)
+                        
+                    Wave(offset: Angle(degrees: self.waveOffset2.degrees), percent: Double(percent)/2000)
+                        .fill(Color(red: 0, green: 0.5, blue: 0.75, opacity: 0.5))
+                        .opacity(0.5)
+                        .frame(width: 300, height: 320)
                 }
+                .mask(
+                    Image("glass")
+                        .interpolation(.none)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 350, height: 350)
+                )
+                .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local).onEnded({ value in
+                    if value.translation.height < 0 {
+                        percent += 250
+                    }
+                    
+                    if value.translation.height > 0 {
+                        percent -= 250
+                    }
+                }))
                 
-                if value.translation.height > 0 {
-                    percent -= 250
-                }
-            }))
-            
-            Text("Adaugă apă glisând în sus paharul")
-                .foregroundColor(Color.secondary)
-        }
-        .navigationTitle("Adaugă apă")
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbar {
-            Button("Adaugă") {
-                logViewModel.addWaterToLog(date: date, water: percent) {
-                    presentationMode.wrappedValue.dismiss()
+                Text("Adaugă apă glisând în sus paharul")
+                    .foregroundColor(Color.secondary)
+            }
+            .navigationTitle("Adaugă apă")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                Button("Adaugă") {
+                    logViewModel.addWaterToLog(date: date, water: percent) {
+                        presentationMode.wrappedValue.dismiss()
+                    }
                 }
             }
+            .onAppear {
+                withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
+                    self.waveOffset = Angle(degrees: 360)
+                    self.waveOffset2 = Angle(degrees: -180)
+                }
         }
-        .onAppear {
-            withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
-                self.waveOffset = Angle(degrees: 360)
-                self.waveOffset2 = Angle(degrees: -180)
-            }
         }
     }
 }
 
 struct AddWaterView_Previews: PreviewProvider {
     static var previews: some View {
-        ZStack {
-            Color.background.ignoresSafeArea(.all)
+        Group {
+            AddWaterView(percent: 250, date: Date().stripTime())
             AddWaterView(percent: 250, date: Date().stripTime())
                 .preferredColorScheme(.dark)
         }
+        
         
     }
 }
