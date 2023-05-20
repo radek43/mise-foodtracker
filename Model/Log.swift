400e9c1216102af6cb236ef65c3b797540b692e7
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-05-20 16:33:35 +0300

    removed log sample data

--- Model/Log.swift
+++ Model/Log.swift
@@ -20,7 +20,7 @@ enum DishCategory: Int, CaseIterable {
 //   - 2: Lunch
 //   - 3: Dinner
 //   - 4: Snacks
-struct DishLog: Identifiable, Codable {
+struct DishLog: Identifiable, Codable, Hashable {
     var id: String = UUID().uuidString
     var mealtype: Int
     var title: String
@@ -49,7 +49,6 @@ struct Log: Identifiable, Codable {
     var water: Double = 0
     
     // MARK: - COMPUTED PROPERTIES
-    
     var totalCalories: Double {
         var sum: Double = 0
         for food in foods {
@@ -129,109 +128,4 @@ struct Log: Identifiable, Codable {
         }
         return sum
     }
-    
-    // MARK: - SAMPLE DATA
-    static var sampleLogs: [Log] = [
-        Log(dateTime: Date.from(year: 2023, month: 5, day: 6),
-            foods: [
-            DishLog(mealtype: 1,
-                    title: "Cartofi prajiti",
-                    servingSize: 100,
-                    calories: 150,
-                    protein: 14,
-                    carbs: 31,
-                    fibers: 0,
-                    fat: 25),
-            DishLog(mealtype: 1,
-                    title: "Ciorbă de cartofi",
-                    servingSize: 100,
-                    calories: 350,
-                    protein: 44,
-                    carbs: 23,
-                    fibers: 0,
-                    fat: 11),
-            DishLog(mealtype: 1,
-                    title: "Cereale cu lapte",
-                    servingSize: 100,
-                    calories: 420,
-                    protein: 32,
-                    carbs: 47,
-                    fibers: 0,
-                    fat: 15)
-        ],
-            activities: [
-                ActivityLog(title: "Alergare", calories: 48, duration: 12),
-                ActivityLog(title: "Haltere", calories: 23, duration: 12),
-                ActivityLog(title: "Plimbare", calories: 12, duration: 12)
-            ],
-            weight: 50,
-            water: 6),
-        
-        Log(dateTime: Date.from(year: 2023, month: 5, day: 7), foods: [
-            DishLog(mealtype: 1,
-                    title: "Pepene roșu",
-                    servingSize: 100,
-                    calories: 250,
-                    protein: 64,
-                    carbs: 13,
-                    fibers: 0,
-                    fat: 21),
-            DishLog(mealtype: 1,
-                    title: "Căpșuni",
-                    servingSize: 100,
-                    calories: 250,
-                    protein: 14,
-                    carbs: 12,
-                    fibers: 0,
-                    fat: 28),
-            DishLog(mealtype: 1,
-                    title: "Alune",
-                    servingSize: 100,
-                    calories: 250,
-                    protein: 24,
-                    carbs: 23,
-                    fibers: 0,
-                    fat: 25)
-        ],
-            activities: [
-                ActivityLog(title: "Plimbare", calories: 140, duration: 12),
-                ActivityLog(title: "Alergare", calories: 140, duration: 12),
-                ActivityLog(title: "Somn", calories: 140, duration: 12)
-            ],
-            weight: 78,
-            water: 3),
-        Log(dateTime: Date.from(year: 2023, month: 5, day: 8), foods: [
-            DishLog(mealtype: 1,
-                    title: "Halva",
-                    servingSize: 100,
-                    calories: 250,
-                    protein: 74,
-                    carbs: 23,
-                    fibers: 0,
-                    fat: 21),
-            DishLog(mealtype: 1,
-                    title: "Ciocolată",
-                    servingSize: 100,
-                    calories: 250,
-                    protein: 36,
-                    carbs: 63,
-                    fibers: 0,
-                    fat: 18),
-            DishLog(mealtype: 1,
-                    title: "Bere",
-                    servingSize: 100,
-                    calories: 50,
-                    protein: 4,
-                    carbs: 13,
-                    fibers: 0,
-                    fat: 45)
-        ],
-            activities: [
-                ActivityLog(title: "Somn", calories: 140, duration: 12),
-                ActivityLog(title: "ALergare", calories: 140, duration: 12),
-                ActivityLog(title: "Haltere", calories: 140, duration: 12)
-            ],
-            weight: 20,
-            water: 1),
-    ]
 }
