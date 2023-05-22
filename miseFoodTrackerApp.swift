c9ee3458dce69adf8bfc8f9a2e43b8a6151102b8
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-05-22 23:03:03 +0300

    removed unused code

--- miseFoodTrackerApp.swift
+++ miseFoodTrackerApp.swift
@@ -10,11 +10,6 @@ import SwiftUI
 
 @main
 struct miseFoodTrackerApp: App {
-    // MARK: - PROPERTIES
-    //    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    //    @StateObject var viewModel = AuthViewModel()
-    
-    // MARK: - BODY
     var body: some Scene {
         WindowGroup {
             LaunchView()
